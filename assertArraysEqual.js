a8d48e1ac4425ea7cf4ac7bdd82b4cc0124a8795
Gary <garyyue2017@gmail.com>
2019-07-23 21:29:37 +0000

    added assertarraysequal function

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`😜 😜 😜 Assertion Passed: ${stringify(arr1)} === ${stringify(arr2)}`);
+    return true;
+  }
+  console.log(`😔 😔 😔 Assertion Failed: ${stringify(arr1)} !== ${stringify(arr2)}`);
+  return false;
+};
+
+const stringify = function(arr) {
+  return JSON.stringify(arr);
+};
+
+let eqArrays = function(arr1, arr2) {
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual(["1","2","3"],["1","2","3"]);
+assertArraysEqual([1,"2",3],[1,"2", "3"]);
