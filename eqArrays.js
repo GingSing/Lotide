d71e2c6d8c7f8d4a33147a64c348082223801fab
Gary <garyyue2017@gmail.com>
2019-07-23 20:53:23 +0000

    added eqArrays function to compare two arrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😜 😜 😜 Assertion Passed: ${addStringQuotes(actual)} === ${addStringQuotes(expected)}`);
+    return true;
+  }
+  console.log(`😔 😔 😔 Assertion Failed: ${addStringQuotes(actual)} !== ${addStringQuotes(expected)}`);
+  return false;
+};
+
+const addStringQuotes = function(word) {
+  if (typeof word === "string") {
+    return `"${word}"`;
+  }
+  return word;
+};
+
+let eqArrays = function(arr1, arr2) {
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
