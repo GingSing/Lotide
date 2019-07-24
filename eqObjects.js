6b47b3ee5920a666397cec6092662421b6d5a945
Gary <garyyue2017@gmail.com>
2019-07-24 22:40:01 +0000

    checked isArray in second object

--- eqObjects.js
+++ eqObjects.js
@@ -31,7 +31,7 @@ const eqObjects = function(object1, object2) {
   }
 
   for(let keys of Object.keys(object1)){
-    if(Array.isArray(object1[keys])){
+    if(Array.isArray(object1[keys]) && Array.isArray(object2[keys])){
       return eqArrays(object1[keys], object2[keys]);
     }else if(object1[keys] !== object2[keys]){
       return false;
